fd2c85bbe4f0fc317c12cf7871805024c3bfa612
Tom <gts-2010@mail.ru>
2023-02-13 16:03:26 +0500

    Initial commit

--- src/main/java/ru/netology/sqr/NumberOfSquares/services/SQRService.java
+++ src/main/java/ru/netology/sqr/NumberOfSquares/services/SQRService.java
@@ -3,10 +3,10 @@ package ru.netology.sqr.NumberOfSquares.services;
 public class SQRService {
 
     public int calcNumberSquares(int maxLimit, int minLimit) {
-        int quantity=0;
-        for (int i=10; i <= 99; i++) {
+        int quantity = 0;
+        for (int i = 10; i <= 99; i++) {
             if ((i * i <= maxLimit) && (minLimit <= i * i)) {
-                quantity=quantity + 1;
+                quantity = quantity + 1;
             }
 
 
--- src/test/java/SQRServiceTest.java
+++ src/test/java/SQRServiceTest.java
@@ -7,32 +7,37 @@ public class SQRServiceTest {
     @Test
 
     public void calcNumberSquares() {
-        SQRService service=new SQRService();
+        SQRService service = new SQRService();
 
-        int expected=3;
-        int actual=service.calcNumberSquares(300, 200);
+        int expected = 3;
+        int actual = service.calcNumberSquares(300, 200);
 
         Assertions.assertEquals(expected, actual);
+
+
     }
 
     @Test
 
     public void calcNumberSquaresOne() {
-        SQRService service=new SQRService();
+        SQRService service = new SQRService();
 
-        int expected=2;
-        int actual=service.calcNumberSquares(121, 8);
+        int expected = 2;
+        int actual = service.calcNumberSquares(121, 8);
 
         Assertions.assertEquals(expected, actual);
+
+
     }
 
     @Test
 
     public void calcNumberSquaresTwo() {
-        SQRService service=new SQRService();
 
-        int expected=8;
-        int actual=service.calcNumberSquares(300, 100);
+        SQRService service = new SQRService();
+
+        int expected = 8;
+        int actual = service.calcNumberSquares(300, 100);
 
         Assertions.assertEquals(expected, actual);
     }
@@ -40,10 +45,10 @@ public class SQRServiceTest {
     @Test
 
     public void calcNumberSquaresThree() {
-        SQRService service=new SQRService();
+        SQRService service = new SQRService();
 
-        int expected=8;
-        int actual=service.calcNumberSquares(300, 10);
+        int expected = 8;
+        int actual = service.calcNumberSquares(300, 10);
 
         Assertions.assertEquals(expected, actual);
     }
@@ -51,10 +56,10 @@ public class SQRServiceTest {
     @Test
 
     public void calcNumberSquaresThreeBorder() {
-        SQRService service=new SQRService();
+        SQRService service = new SQRService();
 
-        int expected=17;
-        int actual=service.calcNumberSquares(1000, 200);
+        int expected = 17;
+        int actual = service.calcNumberSquares(1000, 200);
 
         Assertions.assertEquals(expected, actual);
     }
